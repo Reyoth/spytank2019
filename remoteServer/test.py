72e4f468213139da02141932d347eba89583315f
Reyoth <serialkiwi@gmail.com>
2019-12-18 13:24:51 +0100

    while

--- remoteServer/test.py
+++ remoteServer/test.py
@@ -13,20 +13,21 @@ class detecteur(Thread):
 
     def run(self):
         """Code à exécuter pendant l'exécution du thread."""
-        dist = spytank.litDistance()
-        if dist < 25 :
-            spytank.stop()
-            spytank.led(0,1)
-            spytank.led(1,1)
-            spytank.led(2,1)
-            spytank.led(3,1)
-            self.stop = True
-        else :
-            spytank.led(0,0)
-            spytank.led(1,0)
-            spytank.led(2,0)
-            spytank.led(3,0)
-            self.stop = False
+        while True :
+            dist = spytank.litDistance()
+            if dist < 25 :
+                spytank.stop()
+                spytank.led(0,1)
+                spytank.led(1,1)
+                spytank.led(2,1)
+                spytank.led(3,1)
+                self.stop = True
+            else :
+                spytank.led(0,0)
+                spytank.led(1,0)
+                spytank.led(2,0)
+                spytank.led(3,0)
+                self.stop = False
 
-        print(dist)
+            print(dist)
 
