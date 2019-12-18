7b851d4821c1408b0319f070d5f2f98b3730c79b
Reyoth <serialkiwi@gmail.com>
2019-12-18 12:51:32 +0100

    modif thread

--- remoteServer/server.py
+++ remoteServer/server.py
@@ -2,6 +2,8 @@ import network
 import spytank
 import os
 import time
+import test
+
 ADDRESS="192.168.1.209"
 PORT=1111
 
@@ -10,41 +12,23 @@ socket.bind((ADDRESS,PORT))
 
 continuer = True
 while continuer :
-    dist = spytank.litDistance()
-    print("distance" ,dist)
-    time.sleep(1)
 
-    if dist < 10 :
-        spytank.stop()
-        spytank.led(0,1)
-        spytank.led(1,1)
-        spytank.led(2,1)
-        spytank.led(3,1)
-        #beep
-        print("beep")
-        #os.system("mpg321 beep.wav")
-        time.sleep(1)
+    socket.listen(10)
+    print("en ecoute...")
 
-    else :
-        spytank.led(0,0)
-        spytank.led(1,0)
-        spytank.led(2,0)
-        spytank.led(3,0)
+    thread = network.newThread(socket.accept())
+    thread2 = test.detecteur()
+    thread2.start()
+    thread.start()
 
-        socket.listen(10)
-        print("en ecoute...")
+    #notre communication
+    stop = thread2.run()
+    lettre = thread.clientsocket.recv(4096)
+    lettre = lettre.decode("utf-8")
+    print("message recu : ", lettre)
 
-        thread = network.newThread(socket.accept())
-        thread.start()
-
-        #notre communication
-
-        lettre = thread.clientsocket.recv(4096)
-        lettre = lettre.decode("utf-8")
-        print("message recu : ", lettre)
-
-        vitesse = 255
-
+    vitesse = 255
+    if stop == False :
         if lettre == "z":
             spytank.avance(vitesse)
         elif lettre == "q":
@@ -69,7 +53,7 @@ while continuer :
         elif lettre == "c":
             spytank.stop()
             continuer = False
-        
-        thread.clientsocket.send("j'ai bien recu le message".encode())
+    
+    thread.clientsocket.send("j'ai bien recu le message".encode())
         
 
--- remoteServer/test.py
+++ remoteServer/test.py
@@ -1,1 +1,31 @@
-os.system("beep -f 555 -l 460")
+import random
+import sys
+from threading import Thread
+import time
+import spytank
+
+class detecteur(Thread):
+
+
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        """Code à exécuter pendant l'exécution du thread."""
+        while True :
+            dist = spytank.litDistance()
+            stop = False
+            if dist < 25 :
+                spytank.stop()
+                spytank.led(0,1)
+                spytank.led(1,1)
+                spytank.led(2,1)
+                spytank.led(3,1)
+                stop = True
+            else :
+                spytank.led(0,0)
+                spytank.led(1,0)
+                spytank.led(2,0)
+                spytank.led(3,0)
+            
+            return stop
